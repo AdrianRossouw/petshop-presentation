c456bb5e6a2618836ec0891536df63a505f1a705
Adrian Rossouw <adrian@daemon.co.za>
2012-06-07 19:14:39 +0200

    added static provider.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer();
 
+app.use(express.logger());
+app.use(express.static(__dirname + '/assets'));
+
 app.get('/', function(req, res) {
-   res.send('hello world');
+   res.send('hello worlds');
  });
 
 app.listen(8000);
