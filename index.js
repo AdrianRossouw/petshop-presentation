c4ca775e7e83a26484f533913718aefe0efb853b
Adrian Rossouw <adrian@daemon.co.za>
2012-06-07 19:26:37 +0200

    added basic templates.

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ var app = express.createServer();
 app.use(express.logger());
 app.use(express.static(__dirname + '/assets'));
 
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+
 app.get('/', function(req, res) {
-   res.send('hello worlds');
+  res.render('index', {'target': 'susan'});
  });
 
 app.listen(8000);
